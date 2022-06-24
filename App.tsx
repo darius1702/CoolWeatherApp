afdd9d946117118be3344f37097d64a3236a7cff
Darius Schefer <darius1702@gmail.com>
2022-06-24 19:08:29 +0200

    First basic design

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Button } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.header}>Header</Text>
+            <Text style={styles.mainContent}>Text here</Text>
+            <Button title="Button" />
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        margin: 25
+    },
+    header: {
+        paddingTop: 20,
+        paddingBottom: 20,
+        fontWeight: "bold",
+        fontSize: 40,
+    },
+    mainContent: {
+        fontSize: 20,
+    },
 });
